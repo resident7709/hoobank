5d82dc0265fd5c07a1e0fbbd81c1cbae174f268c
resident7709 <resident7709@gmail.com>
2024-02-25 21:06:33 +0300

    Add CardDeal.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { feedback } from '../constants';
+import styles from '../styles';
+import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+const Testimonials = () => <div>Testimonials</div>;
 
 export default Testimonials;
