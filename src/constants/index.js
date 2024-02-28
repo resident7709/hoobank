c76d4f92f90c1d588fd9d078e747fad957552e17
resident7709 <resident7709@gmail.com>
2024-02-28 20:00:38 +0300

    Add testimonials, CTA sections

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const feedback = [
   {
     id: 'feedback-2',
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      "Money makes your life easier. If you're lucky to have it, you're also lucky to spend it, save it and invest it.",
     name: 'Steve Mark',
     title: 'Founder & Leader',
     img: people02,
