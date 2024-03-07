945a927f11c1fdb2ac8dacdcd29aa9fa4a8db002
resident7709 <resident7709@gmail.com>
2024-03-07 13:19:58 +0300

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  esbuild: {
+    jsx: 'react', // Обрабатывать файлы JSX как React
+  },
 });
